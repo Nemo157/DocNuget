bd5a7ed3976ef149210243a2037a3659c57721ee
Wim Looman <wim@nemo157.com>
2015-07-25 12:24:56 +1200

    Fade out at start of routing then fade back later

--- src/DocNuget/wwwroot/ts/app.ts
+++ src/DocNuget/wwwroot/ts/app.ts
@@ -4,20 +4,23 @@ import emblem from './sammy/emblem'
 import fade from './sammy/fade'
 
 var app = Sammy('#content', app => {
+  app.debug = true
   app.use(emblem, 'em')
   app.use(fade)
   app.setLocationProxy(new PushLocationProxy(app, 'a', 'body'))
 
-  app.get('/', route => route.partial('/views/index.em'))
-  app.get('/about', route => route.partial('/views/about.em'))
-  app.get('/packages/:package', route => route
+  app.get('/', (route, next) => route.partial('/views/index.em').then(next))
+  app.get('/about', (route, next) => route.partial('/views/about.em').then(next))
+  app.get('/packages/:package', (route, next) => route
     .load('/api/packages/' + route.params.package)
     .then(JSON.parse)
-    .partial('/views/package.em'))
-  app.get('/packages/:package/:version', route => route
+    .partial('/views/package.em')
+    .then(next))
+  app.get('/packages/:package/:version', (route, next) => route
     .load('/api/packages/' + route.params.package + '/' + route.params.version)
     .then(JSON.parse)
-    .partial('/views/package.em'))
+    .partial('/views/package.em')
+    .then(next))
 })
 
 app.run()
--- src/DocNuget/wwwroot/ts/sammy/fade.ts
+++ src/DocNuget/wwwroot/ts/sammy/fade.ts
@@ -5,18 +5,48 @@ import NProgress = require('nprogress')
 NProgress.configure({ showSpinner: false })
 
 var fade = (app: Sammy.Application) => {
-  app.swap = function (content: any, callback: any) {
+  var fadeIn = false, fadeOut = false
+  var next: () => void
+
+  var before = () => {
+    fadeOut = true
     NProgress.start()
-    this.$element().fadeOut('fast', () => {
-      NProgress.done()
-      this.$element().html(content)
-      this.$element().fadeIn('fast', () => {
-        if (callback) {
-          callback.apply()
-        }
-      })
+    app.$element().fadeOut('fast', () => {
+      fadeOut = false
+      if (next) {
+        next()
+        next = null
+      }
     })
   }
+
+  var complete = (callback?: () => void) => {
+    if (!fadeOut) {
+      fadeIn = true
+      NProgress.done()
+      app.$element().fadeIn('fast', () => {
+        fadeIn = false
+        if (callback) {
+          callback()
+        }
+      })
+    }
+  }
+
+  var swap = (content: any, callback?: () => void) => {
+    if (fadeOut) {
+      next = () => {
+        app.$element().html(content)
+        complete(callback)
+      }
+    } else {
+      app.$element().html(content)
+    }
+  }
+
+  app.before(before)
+  app.onComplete(() => complete())
+  app.swap = swap
 }
 
 export default fade
